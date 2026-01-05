13244450877bd93045eda0b4b571f4e94456b86d
Thomas Peitz <vorwerk@thomas-peitz.de>
2026-01-05 12:57:59 +0100

    Refactor and simplify

--- go.mod
+++ go.mod
@@ -1,5 +1,7 @@
 module github.com/vorwerk/market-header-wasm
 
-go 1.22
+go 1.24
 
-require github.com/tetratelabs/proxy-wasm-go-sdk v0.24.0
+toolchain go1.24.11
+
+require github.com/proxy-wasm/proxy-wasm-go-sdk v0.0.0-20250212164326-ab4161dcf924
--- main.go
+++ main.go
@@ -1,62 +1,25 @@
 package main
 
 import (
-	"github.com/tetratelabs/proxy-wasm-go-sdk/proxywasm"
-	"github.com/tetratelabs/proxy-wasm-go-sdk/proxywasm/types"
+	"github.com/proxy-wasm/proxy-wasm-go-sdk/proxywasm"
+	"github.com/proxy-wasm/proxy-wasm-go-sdk/proxywasm/types"
 )
 
-// Simple domain to market mapping - avoid complex string operations
-var domainMap = map[string]string{
-	"www.example.com": "test",
-}
-
-func main() {
-	proxywasm.SetVMContext(&vmContext{})
-}
-
-type vmContext struct {
-	types.DefaultVMContext
-}
-
-func (*vmContext) NewPluginContext(contextID uint32) types.PluginContext {
-	return &pluginContext{}
-}
-
-type pluginContext struct {
-	types.DefaultPluginContext
-}
-
-func (ctx *pluginContext) OnPluginStart(pluginConfigurationSize int) types.OnPluginStartStatus {
-	proxywasm.LogInfo("market-header plugin started")
-	return types.OnPluginStartStatusOK
-}
-
-func (*pluginContext) NewHttpContext(contextID uint32) types.HttpContext {
-	return &httpContext{}
+func main() {}
+func init() {
+	// Plugin authors can use any one of four entrypoints, such as
+	// `proxywasm.SetVMContext`, `proxywasm.SetPluginContext`, or
+	// `proxywasm.SetTcpContext`.
+	proxywasm.SetHttpContext(func(contextID uint32) types.HttpContext {
+		return &httpContext{}
+	})
 }
 
 type httpContext struct {
 	types.DefaultHttpContext
 }
 
-func (ctx *httpContext) OnHttpRequestHeaders(numHeaders int, endOfStream bool) types.Action {
-	host, err := proxywasm.GetHttpRequestHeader(":authority")
-	if err != nil {
-		proxywasm.LogWarn("failed to get host")
-		return types.ActionContinue
-	}
-
-	// Simple lookup - avoid string operations that might cause issues
-	if market, ok := domainMap[host]; ok {
-		_ = proxywasm.AddHttpRequestHeader("x-market", market)
-		_ = proxywasm.AddHttpRequestHeader("x-request-market", market)
-		proxywasm.LogInfof("market=%s host=%s", market, host)
-	}
-
-	return types.ActionContinue
-}
-
-func (ctx *httpContext) OnHttpResponseHeaders(numHeaders int, endOfStream bool) types.Action {
-	_ = proxywasm.AddHttpResponseHeader("x-wasm-filter", "market-header")
+func (*httpContext) OnHttpRequestHeaders(numHeaders int, endOfStream bool) types.Action {
+	proxywasm.LogInfo("Hello, world!")
 	return types.ActionContinue
 }
